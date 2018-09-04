c6137af720d3daa30fed1beff96333af416e63fd
Masahiko Ebisuda <ebibibi@gmail.com>
2018-09-04 10:03:51 +0900

    changed sample registration command of task schedule task.

--- RegistTaskSample.ps1
+++ RegistTaskSample.ps1
@@ -1,4 +1,9 @@
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-File 'c:\AzureStackOperationScripts\TestAzureStackAtERCS\Test-AzureStackAtERCS.ps1'"
+#Unregister-ScheduledTask -TaskName TestAzureStackAtERCS
+
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument '-ExecutionPolicy Bypass -File "C:\AzureStackOperationScripts\TestAzureStackAtERCS\Test-AzureStackAtERCS.ps1"'
 $trigger = New-ScheduledTaskTrigger -DaysInterval 1 -Daily -At "7:00 AM"
-$settings = New-ScheduledTaskSettingsSet -Compatibility Win8 -Hidden
-Register-ScheduledTask -TaskPath \ -TaskName TestAzureStackAtERCS -Action $action -Trigger $trigger -Settings $settings
+$settings = New-ScheduledTaskSettingsSet -Compatibility Win8
+Register-ScheduledTask -TaskPath \ -TaskName TestAzureStackAtERCS -Action $action -Trigger $trigger -Settings $settings -User administrator -Password yourpassword
+
+#Start-ScheduledTask -TaskName TestAzureStackAtERCS
+#Get-ScheduledTaskInfo -TaskName TestAzureStackAtERCS
